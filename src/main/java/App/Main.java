91f3e4094e14d5eb8bfbbcc7c840cbcebfed0c3a
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2021-11-25 14:33:37 +0100

    Database connector

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -6,22 +6,43 @@ package App;
 
 //import Helpers.Dialog;
 
+import Core.DatabaseConnector;
+import Core.MySqlDatabaseConnector;
+import Helpers.Log;
+
 /**
  *
  * @author raulsm
  */
 public class Main {
 
+    public static DatabaseConnector dbConnector;
+    
+    // TODO: Insertar en fichero de variables
+    private static String host = "21giin.rsmondejar.com";
+    private static String databaseName = "21giin_presentaciones_digitales_contables";
+    private static String user = "dig_cont_admin";
+    private static String password = "!z9fRr72";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Aplicación iniciada");
+        Log.info("Aplicación iniciada");
         
 //        Dialog.info("Mensaje a pintar", "Mi Título");
 //        Dialog.warning("Mensaje a pintar", "Mi Título");
 //        Dialog.error("Mensaje a pintar", "Mi Título");
-    }
 
+        // TODO: Test connection        
+        try {
+            dbConnector = (DatabaseConnector) new MySqlDatabaseConnector(host, user, password,databaseName);
+            dbConnector.connector();
+            
+            Log.success("Conexión realizada correctamente");
+            
+        } catch (Exception exception) {
+            Log.error(exception);
+        }
+    }
 }
---
+++ src/main/java/Core/DatabaseConnector.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Core;
+
+import Helpers.Log;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author raulsm
+ */
+public abstract class DatabaseConnector implements DatabaseConnectorInterface {
+
+    public Connection connection = null;
+    public static String driver = null;
+    public static String host = null;
+    public static String user = null;
+    public static String password = null;
+    public static String databaseName = null;
+    public static String url = null;
+
+    /**
+     * MySQL Database Connector Constructor.
+     *
+     * @param host Database Host
+     * @param user Database User Name
+     * @param password Database User Password
+     * @param databaseName Database Name
+     */
+    public DatabaseConnector(String host, String user, String password, String databaseName) {
+
+        DatabaseConnector.host = host;
+        DatabaseConnector.user = user;
+        DatabaseConnector.password = password;
+        DatabaseConnector.databaseName = databaseName;
+    }
+
+    @Override
+    public void connector() {
+        try {
+            Class.forName(driver);
+            connection = (Connection) DriverManager.getConnection(url, user, password);
+
+            if (connection == null) {
+                System.out.println("Error database connetion");
+            }
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            Log.error(throwables);
+
+        }
+    }
+}

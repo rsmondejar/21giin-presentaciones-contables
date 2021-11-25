7ea15b449df0d02faa6a4868ed2411f960b3e5fa
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2021-11-25 10:33:12 +0100

    Initial comit

---
+++ src/main/java/App/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package App;
+
+//import Helpers.Dialog;
+
+/**
+ *
+ * @author raulsm
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Aplicación iniciada");
+        
+//        Dialog.info("Mensaje a pintar", "Mi Título");
+//        Dialog.warning("Mensaje a pintar", "Mi Título");
+//        Dialog.error("Mensaje a pintar", "Mi Título");
+    }
+
+}

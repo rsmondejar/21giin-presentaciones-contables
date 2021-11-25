a3eb97ba2a5ad2481a7f5bcb6c65ab37eba158c1
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2021-11-25 10:34:41 +0100

    Add base files to extend

---
+++ src/main/java/App/Controllers/BaseController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App.Controllers;
+
+/**
+ * Base Controller.
+ *
+ * @author raulsm
+ * @version 1.0.0
+ */
+abstract class BaseController {
+
+}
---
+++ src/main/java/App/Views/BaseView.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App.Views;
+
+/**
+ * Base View.
+ *
+ * @author raulsm
+ * @version 1.0.0
+ */
+abstract class BaseView {
+
+}

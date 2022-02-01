fa9182655c9199232fb4b029fc7dcb2b73d4946e
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2022-02-01 11:53:43 +0100

    Base Controller

---
+++ src/main/java/app/controllers/BaseController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.controllers;
+
+import app.entities.User;
+
+/**
+ * Base Controller.
+ *
+ * @author raulsm
+ * @version 1.0.0
+ */
+abstract class BaseController {
+
+    // @TODO: Ver si la dejo abstracta o no
+    // @TODO: Controlar los permisos del usuario
+    private User auth;
+    
+    public BaseController() {
+        System.out.println("Comprobar los permisos del usuario logueado!!!");
+        System.out.println("Definir los permisos del usuario logueado!!!");
+    }
+
+}

804dc5db1547fa740ab8249fa23ef96b5b7b6df1
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2022-02-01 12:07:56 +0100

    Database Connector using mysql driver

---
+++ src/main/java/core/MySqlDatabaseConnector.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author raulsm
+ * @version 1.0.0
+ */
+public class MySqlDatabaseConnector extends DatabaseConnector{
+    
+    public static String mysqlDriver = "com.mysql.cj.jdbc.Driver";
+
+    /**
+     * MySQL Database Connector Constructor.
+     * @param host Database Host
+     * @param user Database User Name
+     * @param password Database User Password
+     * @param databaseName Database Name
+     */
+    public MySqlDatabaseConnector(
+            String host,
+            String user,
+            String password,
+            String databaseName
+    ) {
+        super(host, user, password, databaseName);
+        url = "jdbc:mysql://" + host + "/" + databaseName;
+        
+        MySqlDatabaseConnector.driver = mysqlDriver;
+    }
+}

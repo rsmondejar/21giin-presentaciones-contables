b6ede5f1cd008442c389f203f0c773370265d460
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2022-04-30 14:50:30 +0200

    FIX problemas with path case sensitive

---
+++ src/main/java/app/businesslogic/BaseBusinessLogic.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.businesslogic;
+
+/**
+ * Base Business Logic.
+ *
+ * @author raulsm
+ * @version 1.0.0
+ */
+abstract class BaseBusinessLogic {
+
+}
---
+++ src/main/java/app/models/BaseModel.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.models;
+
+/**
+ * Base Madel
+ * @author raulsm
+ * @version 1.0.0
+ */
+abstract class BaseModel {
+    
+    protected int id;
+    
+    // <editor-fold defaultstate="collapsed" desc="Getters And Setters">
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    // </editor-fold>
+}

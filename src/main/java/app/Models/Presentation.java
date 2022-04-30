8b737791f0d7853d74af3e980795069db01dd240
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2022-04-30 14:44:05 +0200

    Remove files with bad path

--- src/main/java/app/Models/Presentation.java
+++ src/main/java/app/Models/Presentation.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package App.Models;
+package app.models;
 
 import java.util.Date;
 

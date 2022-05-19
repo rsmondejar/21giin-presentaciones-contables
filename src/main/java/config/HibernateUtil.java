fefc3d23f2b68714caf38f881808b74a36fa7387
Raul Sanchez Mondejar <rsanchez@refineriaweb.com>
2022-05-19 16:46:36 +0200

    test integration sonar

--- src/main/java/config/HibernateUtil.java
+++ src/main/java/config/HibernateUtil.java
@@ -31,21 +31,21 @@ public class HibernateUtil {
             
             if (System.getenv("HIBERNATE_URL") != null) {
                 System.out.println("HIBERNATE_URL CAMBIAR!!!");
-                cfg.setProperty("connection.url", System.getenv("HIBERNATE_URL"));
+                cfg.setProperty("hibernate.connection.url", System.getenv("HIBERNATE_URL"));
             } else {
                 System.out.println("HIBERNATE_URL NO SE HA CAMBIADO!!!");
             }
             
             if (System.getenv("HIBERNATE_USERNAME") != null) {
                 System.out.println("HIBERNATE_USERNAME CAMBIAR!!!");
-                cfg.setProperty("connection.username", System.getenv("HIBERNATE_USERNAME"));
+                cfg.setProperty("hibernate.connection.username", System.getenv("HIBERNATE_USERNAME"));
             } else {
                 System.out.println("HIBERNATE_USERNAME NO SE HA CAMBIADO!!!");
             }
             
             if (System.getenv("HIBERNATE_PASSWORD") != null) {
                 System.out.println("HIBERNATE_PASSWORD CAMBIAR!!!");
-                cfg.setProperty("connection.password", System.getenv("HIBERNATE_PASSWORD"));
+                cfg.setProperty("hibernate.connection.password", System.getenv("HIBERNATE_PASSWORD"));
             } else {
                 System.out.println("HIBERNATE_PASSWORD NO SE HA CAMBIADO!!!");
             }
@@ -55,9 +55,9 @@ public class HibernateUtil {
             System.out.println("HIBERNATE_USERNAME: " + System.getenv("HIBERNATE_USERNAME"));
             System.out.println("HIBERNATE_PASSWORD: " + System.getenv("HIBERNATE_PASSWORD"));
             System.out.println("===============================");
-            System.out.println("connection.url: " + cfg.getProperty("connection.url"));
-            System.out.println("connection.username: " + cfg.getProperty("connection.username"));
-            System.out.println("connection.password: " + cfg.getProperty("connection.password"));
+            System.out.println("connection.url: " + cfg.getProperty("hibernate.connection.url"));
+            System.out.println("connection.username: " + cfg.getProperty("hibernate.connection.username"));
+            System.out.println("connection.password: " + cfg.getProperty("hibernate.connection.password"));
             
             System.out.println("===============================");
             
